3bebfe3e05ecf2fee73cf1b0298780f7a8678145
geng0610 <geng0610@gmail.com>
2018-09-18 03:07:15 -0400

    removed stuff

--- public/javascripts/signup-redirect.js
+++
@@ -1,6 +0,0 @@
-// window.onload = function () { setTimeout(function(){
-//     window.location.href = "https://www.swtor.com/r/RhG7QT";
-// },500);  }
-
-
-window.onload = function () { window.location.href = "https://www.swtor.com/r/RhG7QT";  }
